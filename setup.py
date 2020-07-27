ee8afd80d765355eaba429ba40af8ba04f5ccd96
Jeafreezy <jolaiyaemmanuel@gmail.com>
2020-07-27 10:56:12 +0100

    V2 setup file

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as md:
 
 setuptools.setup(
     name="rsgis",
-    version="0.0.1",
-    author="Emmanuel Jolaiya",
-    author_email="jolaiyaemmanuel@gmail.com",
-    description="A python package for basic GIS operations",
+    version="0.0.2",
+    author="Emmanuel Jolaiya, Oke Mattew",
+    author_email="jolaiyaemmanuel@gmail.com, matthewoke16@gmail.com",
+    description="A python package for basic to advanced GIS operations",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeafreezy/rsgis",
